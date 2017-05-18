b00df3805d302c7f96d49e3c6b3fd79cf5165a68
umer <umernasir19@gmail.com>
2017-05-18 09:29:31 +0500

    android

--- app/src/main/java/com/example/hafizumerbinnasir/appointadoctor/Categories.java
+++ app/src/main/java/com/example/hafizumerbinnasir/appointadoctor/Categories.java
@@ -1,24 +1,58 @@
 package com.example.hafizumerbinnasir.appointadoctor;
 
+import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class Categories extends AppCompatActivity{
 
         public ListView listView;
-        String[] listPlanet={"Anatomy","Bones and joints","Child Specialist","Cardiologist","Dentist","Dermatologist","E.N.T","Generel physician","Anatomy","Bones and joints","Child Specialist","Cardiologist","Dentist","Dermatologist","E.N.T","Generel physician"};
+        String[] listPlanet={"\n\\" ,
+                "AUDIOLOGIST\n" ,
+                "BREAST SURGEON\n" ,
+                "CARDIOLOGIST\n" ,
+                "CHEST PHYSICIAN\n" ,
+                "DENTAL SURGEON\n" ,
+                "DERMATOLOGIST\n" ,
+                "DIABETOLOGIST\n" ,
+                "ENDOCRINOLOGIST\n" ,
+                "ENT SURGEON\n" ,
+                "EYE SURGEON\n" ,
+                "GASTROENTEROLOGIST\n" ,
+                "GENERAL PHYSICIAN\n" ,
+                "GENERAL SURGEON\n" ,
+                "GYNAECOLOGIST\n" ,
+                "MAXILLOFASCIAL SURGEON\n" ,
+                "NEPHROLOGIST\n" ,
+                "NEUROSURGEON\n" ,
+                "NEUROLOGIST\n" ,
+                "ORTHODONTIST\n" ,
+                "ORTHOPEDIC SURGEON\n" ,
+                "PAEDIATRIC SURGEON\n" ,
+                "PEDIATRICIAN\n" ,
+                "PHYSIOTHERAPIST\n" ,
+                "PSYCHIATRIST\n" ,
+                "UROLOGIST"};
 
+        @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+        @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_categories);
+            
+
 
             listView = (ListView)findViewById(R.id.lv);
 
